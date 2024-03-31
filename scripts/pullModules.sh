a9caa185e48afc00379308bfa8392d5983c43629
Daniel Hannum <daniel.hannum@gmail.com>
2024-03-30 22:48:03 -0400

    fix

--- scripts/pullModules.sh
+++ scripts/pullModules.sh
@@ -2,7 +2,19 @@
 
 cd ../modules
 git clone git@github.com:MMM-CalendarExt2/MMM-CalendarExt2.git
+cd MMM-CalendarExt2
+npm i
+cd ..
+
 git clone git@github.com:cbrooker/MMM-Todoist.git
+cd MMM-Todoist
+npm i
+cd ..
+
 git clone git@github.com:delightedCrow/WallberryTheme.git
+cd WallberryTheme
+npm i
+cd ..
+
 
 
