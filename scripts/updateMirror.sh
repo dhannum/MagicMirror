195debac1cb8e97ec0d047d45bf1c8c37c8bfec6
Daniel Hannum <daniel.hannum@gmail.com>
2024-06-03 22:03:48 -0400

    better update procedure

--- scripts/updateMirror.sh
+++ scripts/updateMirror.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ..
-git fetch upstream && git pull upstream
+git fetch upstream && git pull upstream master
+git fetch origin && git pull origin master
 npm i
 
 for dir in modules/*; do (cd "$dir" && pwd && git fetch && git pull && npm i); done
