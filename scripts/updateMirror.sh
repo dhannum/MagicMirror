4753ef60a8d5d03ef67a58b8b6119a22cff2d3d8
Daniel Hannum <daniel.hannum@gmail.com>
2024-12-04 12:38:38 -0500

    fix birthdays and script

--- scripts/updateMirror.sh
+++ scripts/updateMirror.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd ..
+echo "**** Merging from upstream"
 git fetch upstream && git pull upstream master
+echo "**** Merging from origin"
 git fetch origin && git pull origin master
+echo "**** Top level npm i"
 npm i
 
-for dir in modules/*; do (echo "$dir" && cd "$dir" && pwd && git fetch && git pull && npm i); done
+for dir in modules/*; do (echo "**** Fetch/Pull/Install $dir" && cd "$dir" && pwd && git fetch && git pull && npm i); done
 
 
