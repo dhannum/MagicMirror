adf64c7925dc70c34f6f805afa0ee7afe385f2ec
Daniel Hannum <daniel.hannum@gmail.com>
2026-01-10 13:55:33 -0500

    tweaks

--- scripts/updateMirror.sh
+++ scripts/updateMirror.sh
@@ -2,12 +2,12 @@
 
 cd ..
 echo "**** Merging from upstream"
-git fetch upstream && git pull upstream master
+git pull upstream master
 echo "**** Merging from origin"
-git fetch origin && git pull origin master
+git pull origin master
 echo "**** Top level npm i"
 npm i
 
-for dir in modules/*; do (echo "**** Fetch/Pull/Install $dir" && cd "$dir" && pwd && git fetch && git pull && npm i); done
+for dir in modules/*; do (echo "**** Fetch/Pull/Install $dir" && cd "$dir" && pwd && git pull && npm i); done
 
 
